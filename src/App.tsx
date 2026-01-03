acabf5471865d9a2f522dde9331f65df878e77d1
Yaroslav <yaroslavlit@gmail.com>
2026-01-04 00:53:34 +0100

    add telegram user data

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@
 
 function App() {
 
+  window.Telegram?.WebApp?.ready();
+  const user = window.Telegram?.WebApp?.initDataUnsafe?.user;
+  const theme = window.Telegram?.WebApp?.themeParams;
+
+
   return (
-   <h1 className="">Hello world</h1>
+    <div>
+      <h1 className="">Hello, {user?.first_name || 'User'}</h1>
+      <p>Your background color: {theme?.bg_color || "Undefined"}</p>
+    </div>
+   
   )
 }
 
